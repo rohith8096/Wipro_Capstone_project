f6b7ebdb33b5136e46b0602c552ddce2df809c1a
rohith8096 <rohithrasoti8096@gmail.com>
2025-09-07 21:57:22 +0530

    final commit

---
+++ Capstone_project/.metadata/.plugins/org.eclipse.core.resources/.history/e8/7087c4dcee6700101b08902eecdcf047
@@ -0,0 +1,15 @@
+package assessmentday3;
+
+public class EvenOddCounter {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int even = 0, odd = 0;
+        for (int num : arr) {
+            if (num % 2 == 0) even++;
+            else odd++;
+        }
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+    }
+}
+
---
+++ Capstone_project/src/test/java/bstack/LoginStep.java
@@ -0,0 +1,36 @@
+package bstack;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStep {
+	WebDriver driver;
+
+@Given("open Bstack website")
+public void open_bstack_website() {
+    driver = new ChromeDriver();
+    driver.get("https://bstackdemo.com/");
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+}
+
+@When("login")
+public void login() {
+    bstackpageclass pg = new bstackpageclass(driver);
+    pg.demologin();
+}
+
+@Then("logged in sucessfully")
+public void logged_in_sucessfully() {
+    System.out.println("login sucessfull");
+    driver.close();
+}
+
+
+}
